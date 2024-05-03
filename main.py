6396e2af7a6b6cb7ff4741f75fc5dc8f24474d75
MIDNIGHT2400 <zinyshen24@gmail.com>
2024-05-03 14:22:52 +0300

    не большие изменения))

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import random
 app = Flask(__name__)
 
 # Список случайных слов
-random_words = ['Привет', 'Мир', 'Фласк', 'Обеспечение']
+random_words = ['Данил клоун', 'Привет СИДОР', 'Бриджейн', 'СЕРЖАН ПРОСЫПАЙСЯ', 'АЛООО БРИДЖИ УКРАЛИ!!!!']
+
 
 @app.route('/')
 def index():
     # Выбор случайного слова из списка
     random_word = random.choice(random_words)
     return render_template('index.html', random_word=random_word)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
