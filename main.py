9184e93bc264bd5ebc653a7d2fadb1f6f05c61fd
Danil222322 <168633774+Danil222322@users.noreply.github.com>
2024-05-03 13:39:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+# Список случайных слов
+random_words = ['Привет', 'Мир', 'Фласк', 'Обеспечение']
+
 @app.route('/')
 def index():
-    return 'Привет, мир!'
+    # Выбор случайного слова из списка
+    random_word = random.choice(random_words)
+    return random_word
 
 if __name__ == '__main__':
-    app.run(debug=True)#
+    app.run(debug=True)
